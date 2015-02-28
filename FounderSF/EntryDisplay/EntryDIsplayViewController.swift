d35b293e0cd62d32c65541e54d058e3b009a7dd5
SamuraiT <t.yasukawa01@gmail.com>
2015-02-28 15:18:53 -0800

    add outlet and redraw button function which draw button nicely with border  #1

--- FounderSF/EntryDisplay/EntryDIsplayViewController.swift
+++ FounderSF/EntryDisplay/EntryDIsplayViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class EntryDisplayViewController: UIViewController {
     
+    @IBOutlet weak var investorOutlet: UIButton!
+    @IBOutlet weak var founderOutlet: UIButton!
+    @IBOutlet weak var entryImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    func drawBorderButton(outlet: UIButton!){
+       outlet.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+       outlet.layer.borderWidth = 3
+       outlet.layer.borderColor = UIColor.whiteColor().CGColor
+       outlet.layer.cornerRadius = 5
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
