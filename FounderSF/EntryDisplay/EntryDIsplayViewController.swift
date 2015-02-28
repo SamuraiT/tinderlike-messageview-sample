96c3365b2ebeab2ed6030989b8a03bcafa31cc76
SamuraiT <t.yasukawa01@gmail.com>
2015-02-27 22:46:46 -0800

    add basic files #1

---
+++ FounderSF/EntryDisplay/EntryDIsplayViewController.swift
@@ -0,0 +1,25 @@
+//
+//  EntryDIsplayView.swift
+//  FounderSF
+//
+//  Created by tatsuro-y on 2015/02/27.
+//  Copyright (c) 2015年 FounderSF. All rights reserved.
+//
+
+import UIKit
+
+class EntryDisplayViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
