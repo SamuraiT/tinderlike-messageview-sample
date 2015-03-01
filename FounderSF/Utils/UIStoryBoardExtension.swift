96fbd034f33fc9f34a7f2e32ca78d3683926b6e0
SamuraiT <t.yasukawa01@gmail.com>
2015-02-28 17:30:18 -0800

    add utility fund which helps to move view #2

---
+++ FounderSF/Utils/UIStoryBoardExtension.swift
@@ -0,0 +1,34 @@
+//
+//  UIStoryBoardExtension.swift
+//  FounderSF
+//
+//  Created by tatsuro-y on 2015/02/28.
+//  Copyright (c) 2015年 FounderSF. All rights reserved.
+//
+
+import UIkit
+
+extension UIStoryboard {
+    class func presentEntryDisplayView(selfView: UIViewController) {
+        moveTo("EntryDisplay", storyboardID: "EntryDisplayViewController", view: selfView)
+    }
+    
+    class func presentFounderInvestorTinderView(selfView: UIViewController) {
+        moveTo("FounderInvestorTinder", storyboardID: "FounderInvestorTinderViewController", view: selfView)
+    }
+    
+    
+    class func presentFounderInvestorTinderNavigationView(selfView: UIViewController) {
+        //FIX
+        //when move to navigation controller, view will be blacked out
+        moveTo("FounderInvestorTinder", storyboardID: "FounderInvestorTinderNavigationController", view: selfView)
+    }
+  
+    class func moveTo(storyboardName:String, storyboardID:String, view:UIViewController){
+        let storyboard:UIStoryboard = UIStoryboard(name: storyboardName, bundle: NSBundle.mainBundle())
+        let distinationView: UIViewController = storyboard.instantiateViewControllerWithIdentifier(storyboardID) as UIViewController
+        view.presentViewController(distinationView, animated: true, completion: nil)
+    
+    }
+  
+}
---
+++ FounderSF/Views/FounderInvestorTinder/FounderInvestorTinderViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FounderInvestorTinderViewController.swift
+//  FounderSF
+//
+//  Created by tatsuro-y on 2015/02/28.
+//  Copyright (c) 2015年 FounderSF. All rights reserved.
+//
+
+import Foundation
