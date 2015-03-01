d4161bda6d301ed18521d320c0aec697116af0fa
SamuraiT <t.yasukawa01@gmail.com>
2015-02-28 17:32:59 -0800

    add basic view and controller for tinder UI view #2

--- FounderSF/Views/FounderInvestorTinder/FounderInvestorTinderViewController.swift
+++ FounderSF/Views/FounderInvestorTinder/FounderInvestorTinderViewController.swift
@@ -6,4 +6,19 @@
 //  Copyright (c) 2015年 FounderSF. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class FounderInvestorTinderViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
