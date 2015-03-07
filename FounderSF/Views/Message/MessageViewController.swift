7f88cdb83d53190c5d9fbad3c61e9b0f491af502
SamuraiT <t.yasukawa01@gmail.com>
2015-03-06 16:15:49 -0800

    add basic message files #4

---
+++ FounderSF/Views/Message/MessageViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MessageViewController.swift
+//  FounderSF
+//
+//  Created by tatsuro-y on 2015/03/06.
+//  Copyright (c) 2015年 FounderSF. All rights reserved.
+//
+
+import UIKit
+
+class MessageViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
